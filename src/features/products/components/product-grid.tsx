93e5aab09ee174296745e118ece5fb0205b156bf
phamthanh <phamhoaithanh32@gmail.com>
2026-05-10 23:08:13 +0700

    Update Document

--- src/features/products/components/product-grid.tsx
+++ src/features/products/components/product-grid.tsx
@@ -193,7 +193,7 @@ export function ProductGrid() {
         )}
 
         {hasProducts && viewMode === 'grid' && (
-          <div className='mt-4 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          <div className='mt-4 grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
             {data.products.map((product) => (
               <ProductCard key={product.id} product={product} />
             ))}
